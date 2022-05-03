5f2f7da320362a8bd7a0389573fcbbab7bed3f1c
marcosemilson <marcosenpereira@gmail.com>
2022-05-03 19:36:56 -0300

    Mapeamento JPA

---
+++ src/main/java/com/dio/santader/banckline/api/model/Conta.java
@@ -0,0 +1,31 @@
+package com.dio.santader.banckline.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Embeddable
+public class Conta {
+
+    @Column(name = "conta_numero")
+    private Long numero;
+    @Column(name = "conta_saldo")
+    private Double saldo;
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/com/dio/santader/banckline/api/model/MovimentacaoTipo.java
@@ -0,0 +1,6 @@
+package com.dio.santader.banckline.api.model;
+
+public enum MovimentacaoTipo {
+    RECEITA,
+    DESPESA
+}
