41a6af36ee77e4626f6a46cdbaaca40d93885518
marcosemilson <marcosenpereira@gmail.com>
2022-05-03 18:15:49 -0300

    Initial commit

---
+++ src/test/java/com/dio/santader/banckline/api/BancklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santader.banckline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
